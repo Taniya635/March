d1d6a231eab12ba4e046f17429058636376dba36
taniya635 <kumara2138@gmail.com>
2025-03-02 16:32:52 +0530

    2-march

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
-// #include <stdio.h>
-// int main(){
-//     int a, b, sum;
-//     printf("Enter the number: ");
-//     scanf("%d %d",&a, &b);
-//     sum = a+b;
-//     printf("The sum of %d and %d is %d\n:",a,b,sum);
-//     return 0; 
-// }
+#include <stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Enter the number: ");
+    scanf("%d %d",&a, &b);
+    sum = a+b;
+    printf("The sum of %d and %d is %d\n:",a,b,sum);
+    return 0; 
+}
 
 #include <stdio.h>
 
