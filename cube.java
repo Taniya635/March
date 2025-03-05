58ec01cef0f4054de57a23665ce24eba43361ab8
taniya635 <kumara2138@gmail.com>
2025-03-05 23:23:21 +0530

    5-march

---
+++ cube.java
@@ -0,0 +1,8 @@
+public class cube {
+    public static void main(String[] args) {
+        int num, cu;
+        num=Integer.parseInt(args[0]);
+        cu=num*num*num;
+        System.out.println("Cube is:"+cu);
+    }
+}
